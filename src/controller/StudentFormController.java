9111bb344486c799f0d5adc688865a030148959a
isneha44 <isneha44@gmail.com>
2022-07-12 11:38:55 +0530

    Add Comphonents to the student controller

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,4 +1,38 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class StudentFormController {
+    public AnchorPane StudentContext;
+    public JFXTextField txtSid;
+    public JFXTextField txtSName;
+    public TextField txtSearch;
+    public JFXTextField txtContact;
+    public JFXTextField txtSemail;
+    public JFXTextField txtAdress;
+    public JFXButton btnSaveStudent;
+    public JFXButton btnNewStudent;
+
+    public void EnterKeyRelease(KeyEvent keyEvent) {
+    }
+
+    public void SaveStudentrOnAction(ActionEvent actionEvent) {
+    }
+
+    public void NewStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void DeleteStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void UpdateStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void SearchStudentOnAction(ActionEvent actionEvent) {
+    }
 }
